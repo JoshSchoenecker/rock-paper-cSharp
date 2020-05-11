9e84b324674bf8b7b143d775f0f54d420f52d5fc
Josh Schoenecker <schoeneckerj@gmail.com>
2020-05-11 16:09:06 -0600

    computerChoice resetting-Win/Lose not working

--- Program.cs
+++ Program.cs
@@ -15,9 +15,11 @@ namespace rockPaperScissors
             bool playing = userInput.ToLower() == "y";
 
             // NOTE Data binding
+            int wins = 0;
+            int losses = 0;
             Random random = new Random();
             int computerChoice = random.Next(1, 3);
-            
+
             // NOTE Game
             while (playing)
             {
@@ -29,16 +31,66 @@ namespace rockPaperScissors
                     if (playerChoice == "Rock")
                     {
                         System.Console.WriteLine("The computer chose rock as well, It is a tie!");
+
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("It's a tie! Do you want to play again? Y/N");
+                        string choice = Console.ReadLine();
+                        if (choice.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else if (playerChoice == "Paper")
                     {
                         System.Console.WriteLine("The computer chose rock");
                         System.Console.WriteLine("You Win! paper beats rock");
+
+                        wins++;
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("You won! Do you want to play again? Y/N");
+                        string continueGame = Console.ReadLine();
+                        if (continueGame.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else if (playerChoice == "Scissors")
                     {
                         System.Console.WriteLine("The computer chose rock");
                         System.Console.WriteLine("You Lose! rock beats scissors");
+
+                        losses++;
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("You Lost! Do you want to play again? Y/N");
+                        string choice = Console.ReadLine();
+                        if (choice.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else
                     {
@@ -51,15 +103,65 @@ namespace rockPaperScissors
                     {
                         System.Console.WriteLine("The computer chose paper");
                         System.Console.WriteLine("You Lose! paper beats rock");
+
+                        losses++;
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("You Lost! Do you want to play again? Y/N");
+                        string choice = Console.ReadLine();
+                        if (choice.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else if (playerChoice == "Paper")
                     {
                         System.Console.WriteLine("The computer chose paper as well, It is a tie!");
+
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("It's a tie! Do you want to play again? Y/N");
+                        string choice = Console.ReadLine();
+                        if (choice.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else if (playerChoice == "Scissors")
                     {
                         System.Console.WriteLine("The computer chose paper");
                         System.Console.WriteLine("You Win! scissors beats paper");
+
+                        wins++;
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("You won! Do you want to play again? Y/N");
+                        string continueGame = Console.ReadLine();
+                        if (continueGame.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else
                     {
@@ -72,15 +174,65 @@ namespace rockPaperScissors
                     {
                         System.Console.WriteLine("The computer chose scissors");
                         System.Console.WriteLine("You Win! rock beats scissors");
+
+                        wins++;
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("You won! Do you want to play again? Y/N");
+                        string continueGame = Console.ReadLine();
+                        if (continueGame.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else if (playerChoice == "Paper")
                     {
                         System.Console.WriteLine("The computer chose scissors");
                         System.Console.WriteLine("You Lose! scissors beats paper");
+
+                        losses++;
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("You Lost! Do you want to play again? Y/N");
+                        string choice = Console.ReadLine();
+                        if (choice.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else if (playerChoice == "Scissors")
                     {
                         System.Console.WriteLine("The computer chose scissors as well, It is a tie!");
+
+                        System.Console.WriteLine("Wins: ", wins);
+                        System.Console.WriteLine("Losses: ", losses);
+
+                        System.Console.WriteLine("It's a tie! Do you want to play again? Y/N");
+                        string choice = Console.ReadLine();
+                        if (choice.ToLower() == "y")
+                        {
+                            computerChoice = random.Next(1, 3);
+                            continue;
+                        }
+                        else
+                        {
+                            playing = false;
+                        }
+                        continue;
                     }
                     else
                     {
